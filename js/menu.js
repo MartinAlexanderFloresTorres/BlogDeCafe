f4f936dc8962b8dda738c4ee18d65528ec2e574e
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-02-10 21:53:43 -0500

    agregamos una nueva funcion para el thema de color del body

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,42 @@
 const menudespegable = document.querySelector(".menubar");
 const navegacion = document.querySelector(".nav");
-const bg = document.querySelector('.bg');
+const bg = document.querySelector(".bg");
 
 menudespegable.addEventListener("click", () => {
     menudespegable.classList.toggle("active");
-    navegacion.classList.toggle('active');
+    navegacion.classList.toggle("active");
 });
-document.addEventListener('scroll', () =>{
+document.addEventListener("scroll", () => {
     const valor_scroll = window.scrollY;
     if (valor_scroll > 100) {
         bg.style.backgroundColor = "#000000";
     } else {
         bg.style.backgroundColor = "#0000003f";
     }
-})
+});
+
+// menu de color
+
+const tema_iconos = document.querySelector(".tema-iconos");
+const body = document.querySelector('body');
+
+tema_iconos.addEventListener("click", () => {
+    tema_iconos.classList.toggle('active');
+    body.classList.toggle('claro');
+
+    if (!body.classList.contains('claro')) {
+        localStorage.setItem('modo', 'oscuro')
+    } else {
+        localStorage.setItem('modo', 'claro');
+    }
+});
+
+const getmodo = localStorage.getItem('modo');
+
+if (getmodo === ('claro')) {
+    body.classList.toggle('claro');
+    tema_iconos.classList.toggle('active');
+} else {
+    body.classList.remove('claro');
+    tema_iconos.classList.remove('active');
+}
